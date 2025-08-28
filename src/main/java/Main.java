452fc8283657bcebc904636f8773d0204a15ff12
Levi Goossens <Levi.Goossens@student.pxl.be>
2025-08-28 17:27:53 +0200

    clear project

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import olivia.Window;
-
-public class Main {
-    public static void main(String[] args) {
-        Window window = Window.get();
-        window.run();
-    }
-}
--- src/main/java/olivia/GameObject.java
+++
@@ -1,4 +0,0 @@
-package olivia;
-
-public class GameObject {
-}
