8c1955fa52f3b88eda40ef2c33f8fc3ede1deea7
Levi Goossens <Levi.Goossens@student.pxl.be>
2025-09-01 11:30:43 +0200

    use camera in scene

--- src/main/java/saphire/LevelEditorScene.java
+++ src/main/java/saphire/LevelEditorScene.java
@@ -1,5 +1,6 @@
 package saphire;
 
+import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL20;
 import renderer.Shader;
@@ -38,10 +39,10 @@ public class LevelEditorScene extends Scene {
 
     private float[] vertexArray = {
             // position             // color
-            0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f, 1.0f, // bottom right     0
-            -0.5f, 0.5f, 0.0f,      0.0f, 1.0f, 0.0f, 1.0f, // top left         1
-            0.5f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f, 1.0f, // top right        2
-            -0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f, // bottom left      3
+            50.0f, -50.0f, 0.0f,      1.0f, 0.0f, 0.0f, 1.0f, // bottom right     0
+            -50.0f, 50.0f, 0.0f,      0.0f, 1.0f, 0.0f, 1.0f, // top left         1
+            50.0f, 50.0f, 0.0f,       0.0f, 0.0f, 1.0f, 1.0f, // top right        2
+            -50.0f, -50.0f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f, // bottom left      3
     };
 
     // IMPORTANT: Must be in counter-clockwise order
@@ -77,6 +78,7 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
+        this.camera = new Camera(new Vector2f());
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compileAndLink();
 
@@ -111,7 +113,10 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void update(float dt) {
+        camera.position.x -= dt * 50.0f;
         defaultShader.use();
+        defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
+        defaultShader.uploadMat4f("uView", camera.getViewMatrix());
         glBindVertexArray(vaoID);
 
         //glEnableVertexAttribArray(0); // seemingly needed in tutorial, seemingly unneeded according to chatGPT and running program
--- src/main/java/saphire/Scene.java
+++ src/main/java/saphire/Scene.java
@@ -2,6 +2,8 @@ package saphire;
 
 public abstract class Scene {
 
+    protected Camera camera;
+
     public Scene() {
 
     }
