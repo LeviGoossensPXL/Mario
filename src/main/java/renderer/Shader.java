6ec8465e0902ff865250a991097246a4dc16cc67
Levi Goossens <Levi.Goossens@student.pxl.be>
2025-09-07 20:04:29 +0200

    make sure we are using the correct shader

--- src/main/java/renderer/Shader.java
+++ src/main/java/renderer/Shader.java
@@ -15,6 +15,7 @@ import static org.lwjgl.opengl.GL30.glBindVertexArray;
 
 public class Shader {
     private int shaderProgramID;
+    private boolean beingUsed = false;
 
     private String vertexSource;
     private String fragmentSource;
@@ -65,15 +66,20 @@ public class Shader {
     }
 
     public void use() {
-        glUseProgram(shaderProgramID);
+        if (!beingUsed) {
+            glUseProgram(shaderProgramID);
+            beingUsed = true;
+        }
     }
 
     public void detach() {
         glUseProgram(0);
+        beingUsed = false;
     }
 
     public void uploadMat4f(String varName, Matrix4f mat4) {
         int varLocation = glGetUniformLocation(shaderProgramID, varName);
+        use();
         FloatBuffer matBuffer = BufferUtils.createFloatBuffer(16);
         mat4.get(matBuffer);
         glUniformMatrix4fv(varLocation, false, matBuffer);
