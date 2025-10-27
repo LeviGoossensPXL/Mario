9ee991a5cd0ff369038955bd9a4363b425f55b26
Levi Goossens <Levi.Goossens@student.pxl.be>
2025-10-27 18:09:55 +0100

    create renderer and render batch classes

---
+++ src/main/java/renderer/RenderBatch.java
@@ -0,0 +1,226 @@
+package renderer;
+
+import components.FontRenderer;
+import components.SpriteRenderer;
+import lombok.Getter;
+import org.joml.Vector2f;
+import org.joml.Vector4f;
+import org.lwjgl.BufferUtils;
+import saphire.Camera;
+import saphire.GameObject;
+import saphire.Window;
+
+import java.io.File;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+public class RenderBatch {
+    // Vertex
+    // ===========
+    // Pos              Color
+    // float, float,    float, float, float, float
+    private final int POS_SIZE = 2;
+    private final int COLOR_SIZE = 4;
+
+    private final int POS_OFFSET = 0;
+    private final int COLOR_OFFSET = POS_OFFSET + POS_SIZE * Float.BYTES;
+    private final int VERTEX_SIZE = 6;
+    private final int VERTEX_SIZE_BYTES = VERTEX_SIZE * Float.BYTES;
+
+    private SpriteRenderer[] sprites;
+    private int numSprites;
+    @Getter
+    private boolean hasRoom;
+    private float[] vertices;
+
+    private int vaoID, vboID;
+    private int maxBatchSize;
+    private Shader shader;
+
+    public RenderBatch(int maxBatchSize) {
+        shader = new Shader("assets/shaders/default.glsl");
+        shader.compileAndLink();
+        this.sprites = new SpriteRenderer[maxBatchSize];
+        this.maxBatchSize = maxBatchSize;
+
+        // 4 vertices quads
+        vertices = new float[maxBatchSize * 4 * VERTEX_SIZE];
+        this.numSprites = 0;
+        this.hasRoom = false;
+    }
+
+    public void start() {
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, (long) vertices.length * Float.BYTES, GL_DYNAMIC_DRAW);
+
+        int eboID = glGenBuffers();
+        int[] indices = generateIndices();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+
+        glVertexAttribPointer(0, POS_SIZE, GL_FLOAT, false, VERTEX_SIZE_BYTES, POS_OFFSET);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, COLOR_SIZE, GL_FLOAT, false, VERTEX_SIZE_BYTES, COLOR_OFFSET);
+        glEnableVertexAttribArray(1);
+    }
+
+    public void addSprite(SpriteRenderer sprite) {
+        int index = this.numSprites;
+        this.sprites[index] = sprite;
+        this.numSprites++;
+
+        loadVertexProperties(index);
+
+        if (numSprites >= maxBatchSize) {
+            hasRoom = false;
+        }
+    }
+
+    private void loadVertexProperties(int index) {
+        SpriteRenderer sprite = sprites[index];
+
+        int offset = index * 4 * VERTEX_SIZE;
+
+        Vector4f color = sprite.getColor();
+
+        float xAdd = 1.0f;
+        float yAdd = 1.0f;
+        for (int i = 0; i < 4; i++) {
+            if (i == 1) {
+                yAdd = 0.0f;
+            } else if (i == 2) {
+                xAdd = 0.0f;
+            } else if (i == 3) {
+                yAdd = 1.0f;
+            }
+        }
+
+        vertices[offset] = sprite.gameObject.transform.position.x + (xAdd * sprite.gameObject.transform.scale.x);
+        vertices[offset + 1] = sprite.gameObject.transform.position.y + (yAdd * sprite.gameObject.transform.scale.y);
+
+        vertices[offset + 2] = color.x;
+        vertices[offset + 3] = color.y;
+        vertices[offset + 4] = color.z;
+        vertices[offset + 5] = color.w;
+
+        offset += VERTEX_SIZE;
+    }
+
+    public void render() {
+        // TODO For now we rebuffer all data every frame
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, vertices);
+
+        shader.use();
+        shader.uploadMat4f("uProjection", Window.getCurrentScene().getCamera().getProjectionMatrix());
+        shader.uploadMat4f("uView", Window.getCurrentScene().getCamera().getViewMatrix());
+
+        glBindVertexArray(vaoID);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, numSprites * 6, GL_UNSIGNED_INT, 0);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+
+        shader.detach();
+    }
+
+    private int[] generateIndices() {
+        int[] elements = new int[6 * maxBatchSize];
+
+        for (int i = 0; i < elements.length; i++) {
+            loadElementIndices(elements, i);
+        }
+
+        return elements;
+    }
+
+    private void loadElementIndices(int[] elements, int index) {
+        int offsetArrayIndex = 6 * index;
+        int offset = 4 * index;
+
+        // 3, 2, 0, 0, 2, 1
+        // Triangle 1
+        elements[offsetArrayIndex] = offset + 3;
+        elements[offsetArrayIndex + 1] = offset + 2;
+        elements[offsetArrayIndex + 2] = offset;
+
+        // Triangle 2
+        elements[offsetArrayIndex + 3] = offset;
+        elements[offsetArrayIndex + 4] = offset + 2;
+        elements[offsetArrayIndex + 5] = offset + 1;
+    }
+
+    @Override
+    public void init() {
+        System.out.println("creating test object 1");
+        testObject = new GameObject("test object 1");
+        testObject.addComponent(new FontRenderer());
+        testObject.addComponent(new SpriteRenderer());
+        addGameObjectToScene(this.testObject);
+
+        this.camera = new Camera(new Vector2f());
+        defaultShader = new Shader("assets/shaders/default.glsl");
+        defaultShader.compileAndLink();
+        this.testTexture = new Texture(new File("assets/images/testImage.png")); //test texture
+
+
+    }
+
+    @Override
+    public void update(float dt) {
+        camera.position.x -= dt * 50.0f;
+        defaultShader.use();
+
+        defaultShader.uploadTexture("TEX_SAMPLER", 0);
+        glActiveTexture(GL_TEXTURE0);
+        testTexture.bind();
+
+        defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
+        defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        glBindVertexArray(vaoID);
+
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
+
+        glBindVertexArray(0);
+
+        defaultShader.detach();
+        if (firstTime) {
+            System.out.println("creating test object 2");
+            GameObject go1 = new GameObject("test object 2");
+            go1.addComponent(new SpriteRenderer());
+            addGameObjectToScene(go1);
+            firstTime = false;
+        }
+
+        for (GameObject gameObject : gameObjects) {
+            gameObject.update(dt);
+        }
+    }
+}
---
+++ src/main/java/renderer/Renderer.java
@@ -0,0 +1,47 @@
+package renderer;
+
+import components.SpriteRenderer;
+import saphire.GameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Renderer {
+
+    private final int MAX_BATCH_SIZE = 1000;
+    private List<RenderBatch> batches;
+
+    public Renderer() {
+        this.batches = new ArrayList<>();
+    }
+
+    public void add(GameObject gameObject) {
+        SpriteRenderer spriteRenderer = gameObject.getComponent(SpriteRenderer.class);
+        if (spriteRenderer != null) {
+            add(spriteRenderer);
+        }
+    }
+
+    private void add(SpriteRenderer spriteRenderer) {
+        boolean added = false;
+        for (RenderBatch renderBatch : batches) {
+            if (renderBatch.isHasRoom()) {
+                renderBatch.addSprite(spriteRenderer);
+                added = true;
+                break;
+            }
+        }
+        if (!added) {
+            RenderBatch newBatch = new RenderBatch(MAX_BATCH_SIZE);
+            newBatch.start();
+            batches.add(newBatch);
+            newBatch.addSprite(spriteRenderer);
+        }
+    }
+
+    public void render() {
+        for (RenderBatch renderBatch : batches) {
+            renderBatch.render();
+        }
+    }
+}
