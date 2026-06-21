b7bd06e601b799a038ed11b2f91f722107588d6f
levig <levi.goossens107@gmail.com>
2026-06-21 14:09:23 +0200

    add texture to SpriteRenderer

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -1,16 +1,28 @@
 package components;
 
 import lombok.Getter;
+import org.joml.Vector2f;
 import org.joml.Vector4f;
+import renderer.Texture;
 import saphire.Component;
 
 public class SpriteRenderer extends Component {
 
-    public  SpriteRenderer(Vector4f color) {
-        this.color = color;
-    }
     @Getter
     private Vector4f color;
+    private Vector2f[] textCoords;
+    @Getter
+    private Texture texture;
+
+    public  SpriteRenderer(Vector4f color) {
+        this.texture = null;
+        this.color = color;
+    }
+
+    public  SpriteRenderer(Texture texture) {
+        this.texture = texture;
+        this.color = new Vector4f(1, 1, 1, 1);
+    }
 
     @Override
     public void start() {
@@ -20,5 +32,14 @@ public class SpriteRenderer extends Component {
     @Override
     public void update(float dt) {
 
+    }
+
+    public Vector2f[] getTextCoords() {
+        return new Vector2f[]{
+                new Vector2f(1, 1),
+                new Vector2f(1, 0),
+                new Vector2f(0, 1),
+                new Vector2f(0, 0),
+        };
     }
 }
